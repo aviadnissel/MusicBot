4b94fb3981e5ca7a174c95613ed1f0bcc73c8049
avian <aviadnissel@gmail.com>
2016-09-15 16:34:15 +0300

    fixes

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -398,7 +398,8 @@ class MusicBot(discord.Client):
             else:
                 newmsg = 'Now playing in %s: **%s**' % (
                     player.voice_client.channel.name, entry.title)
-            newmsg += "\nComing up next - %s" % player.playlist.peek()
+            if player.playlist.peek():
+                newmsg += "\nComing up next - %s" % player.playlist.peek().title
             if self.server_specific_data[channel.server]['last_np_msg']:
                 self.server_specific_data[channel.server]['last_np_msg'] = await self.safe_edit_message(last_np_msg, newmsg, send_if_fail=True)
             else:
